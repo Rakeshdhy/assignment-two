449df7a152a3db43875dd94bdbd4fa6ad3479af7
Rakeshdhy <rakeshsondagar@gmail.com>
2024-02-01 14:24:51 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let popUp = document.getElementById("popup");
+
+function openPopup() {
+  popUp.classList.add("open_popup");
+}
+
+function closePopup() {
+  popUp.classList.remove("open_popup");
+}
